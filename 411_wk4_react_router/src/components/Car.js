6d5bc673797ca4456da38b583b8acba1d3a9b17a
unknown <xander.mcgowan@gmail.com>
2023-08-19 16:55:08 -0500

    assignment complete

--- 411_wk4_react_router/src/components/Car.js
+++ 411_wk4_react_router/src/components/Car.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import cars from '../cars.json'
-import { useParams } from "react-router-dom"
+import React from "react";
+import cars from "../cars.json";
+import { useParams } from "react-router-dom";
 
 // import MUI components here //
-import Paper from '@mui/material/Paper'
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
+import Chip from "@mui/material/Chip";
 
 const Car = () => {
-    const {id} = useParams()
-    const car = cars[id-1]
+  const { id } = useParams();
+  const car = cars[id - 1];
 
-
-    return (
-        <>
+  return (
+    <Container fixed style={{width: "30vw"}}>
+      <Paper elevation={3}>
         <h1> {car.Name}</h1>
-        <h1> {car.Cylinders}</h1>
-        
-        </>
-    )
-}
+        <Chip label={`Id: ${car.id}`} style={{margin: "0.25vh"}}/>
+        <Chip label={`Name: ${car.Name}`} style={{margin: "0.25vh"}}/>
+        <Chip label={`Miles Per Gallon: ${car.Miles_per_Gallon}`} style={{margin: "0.25vh"}}/>
+        <Chip label={`Cylinders: ${car.Cylinders}`} style={{margin: "0.25vh"}}/>
+        <Chip label={`Displacement: ${car.Displacement}`} style={{margin: "0.25vh"}}/>
+        <Chip label={`Horsepower: ${car.Horsepower}`} style={{margin: "0.25vh"}}/>
+        <Chip label={`Weight (lbs): ${car.Weight_in_lbs}`} style={{margin: "0.25vh"}}/>
+        <Chip label={`Acceleration: ${car.Acceleration}`} style={{margin: "0.25vh"}}/>
+        <Chip label={`Orgin: ${car.Orgin}`} style={{margin: "0.25vh"}}/>
+      </Paper>
+    </Container>
+  );
+};
 
-export default Car
+export default Car;
